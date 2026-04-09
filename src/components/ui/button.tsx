d2062cdf780b1093f98294ecd1e64b68a6a32d62
Jacob Moy <apixelatedpug@gmail.com>
2026-04-09 11:49:42 -0700

    fix(ui): 🐛 re-add disabled button variant

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -21,6 +21,8 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
         selected:
           "bg-selected text-primary-foreground shadow-xs hover:bg-selected/90",
+        disabled:
+          "border bg-background shadow-xs text-zinc-500 dark:bg-input/20 dark:border-input dark:text-zinc-400 hover:cursor-not-allowed",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
